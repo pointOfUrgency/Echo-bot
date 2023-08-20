fb8611a36eef0d5c0216eca1c6026b6c44614a89
Urg <gunkogtr@gmal.com>
2023-08-20 16:38:05 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+
+
+bot = telebot.TeleBot("6636731338:AAF4x78pPLBb3PPsWkdLSRktgXkPF0He5qI")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hi! I am a test bot. I created by Urg.')
+
+
+@bot.message_handler()
+def echo(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
